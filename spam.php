7b3c7ec211be8c797260fc920a6ff7dad318b289
Axera <55115160+Exccr@users.noreply.github.com>
2022-10-27 14:46:30 +0700

    Update spam.php

--- spam.php
+++ spam.php
@@ -6,9 +6,17 @@ echo "
   / /_/ / / / / __  / /| /_  /
  / _, _/ /_/ / /_/ / ___ |/ /_
 /_/ |_|\__,_/\__,_/_/  |_/___/
+\n
+**************************************
+*                                    *
+* Github : github.com/Exccr          *
+* Facebook : fb.com/rud.az.9         *
+* Instagram : instagram.com/rud.az_  *
+*                                    *
+**************************************
 \n\n";
 
-function spamCall($api, $nomer, $jumlah) {
+function spamCall($api, $nomer, $jumlah, $delay) {
 	$url = "https://spamertelpon.herokuapp.com/v1?key=". $api ."&target=". $nomer;
 	$loop = 0;
 	while ($loop < $jumlah) {
@@ -18,21 +26,26 @@ function spamCall($api, $nomer, $jumlah) {
 		$response = curl_exec($curl);
 		curl_close($curl);
 		$res = json_decode($response, true);
-		if ($res['status'] !== "success") {
+		if ($res['status'] !== true) {
 			echo "Minimal beli Api Key! \n";
 		} else {
-			echo "Spam ke " . $loop . " berhasil dikirim! \n";
+			echo "Spam ke " . $loop + 1 . " berhasil dikirim! \n";
 		}
-		sleep(30);
+		sleep($delay);
 		$loop++;
 	}
 }
 
 echo "Masukan Nomor : ";
 $nomor = trim(fgets(STDIN));
-echo "Masukan Jumlah : ";
+echo "\nMasukan Jumlah : ";
 $jumlah = trim(fgets(STDIN));
-echo "Masukan Key : ";
+echo "\nMasukan jeda : ";
+$delay = trim(fgets(STDIN));
+if (!$delay) {
+	$delay = "30";
+}
+echo "\nMasukan Key : ";
 $key = trim(fgets(STDIN));
 
-spamCall($key, $nomor, $jumlah);
+spamCall($key, $nomor, $jumlah, $delay);
